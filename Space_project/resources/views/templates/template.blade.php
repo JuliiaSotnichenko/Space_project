1155139724f021d08c3fd34003e3d914b39d3e7e
Vichitaev <78409677+Vichitaev@users.noreply.github.com>
2021-05-17 12:23:51 +0200

    a

--- Space_project/resources/views/templates/template.blade.php
+++ Space_project/resources/views/templates/template.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,39 +9,42 @@
 
     @yield('style')
 </head>
+
 <body>
     <main>
         <nav class="main__navig">
-          <img class="logo" src="/images/logoSF.svg" alt="Starlight logo">
-        <ul class="main__navig_list">
-            <li><a href="{{ url('home') }}">Home</a></li>
-            <li><a href="{{ url('menu') }}">Menu</a></li>
-            <li><a href="{{ url('about us') }}">About us</a></li>
-            <li><a href="{{ url('history') }}">History</a></li>
-            <li><a href="{{ url('contact') }}">Contact</a></li>
-        </ul>
-        <ul>
-            <li><a href="{{ url('home') }}">Login/Register</a></li>
-        </ul>
+            <img class="logo" src="/images/logoSF.svg" alt="Starlight logo">
+            <ul class="main__navig_list">
+                <li><a href="{{ url('home') }}">Home</a></li>
+                <li><a href="{{ url('menu') }}">Menu</a></li>
+                <li><a href="{{ url('about us') }}">About us</a></li>
+                <li><a href="{{ url('history') }}">History</a></li>
+                <li><a href="{{ url('contact') }}">Contact</a></li>
+            </ul>
+            <ul>
+                <li><a href="{{ route('login') }}">Login</a></li>
+                <li><a href="{{ route('register') }}">Register</a></li>
+            </ul>
         </nav>
 
-    <div class="content">
-        @yield('content')
-    </div>
-    
-    <div class="content2">
-        @yield('content2')
-        
-    </div>
-</main>
+        <div class="content">
+            @yield('content')
+        </div>
+
+        <div class="content2">
+            @yield('content2')
+
+        </div>
+    </main>
 
     <footer>
-    <div id="copyright text-right">© Copyright 2021 </div>
+        <div id="copyright text-right">© Copyright 2021 </div>
     </footer>
-    
+
     @yield('script')
 
 
-   
+
 </body>
+
 </html>
