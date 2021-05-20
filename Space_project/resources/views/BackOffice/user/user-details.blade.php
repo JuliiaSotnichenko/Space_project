09858a20afc48850746f4a662fee5c435baebaf4
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2021-05-20 11:35:39 +0200

    Update user-details.blade.php

--- Space_project/resources/views/BackOffice/user/user-details.blade.php
+++ Space_project/resources/views/BackOffice/user/user-details.blade.php
@@ -12,7 +12,7 @@
     <li>Country: {{ $user->country }}</li>
     <li>Training Status: {{ $user->training_status }}</li>
     <!-- User's info edit button -->
-    <li><a href="{{ url("/admin/edit/user/", $user->id) }}">Edit User's informations</a></li>
+    <li><a href="{{ url('/admin/edit/user', $user->id) }}">Edit User's informations</a></li>
     <!-- Delete User button -->
     <li><button class="deleteButton" value="{{$user->id}}">Delete</button></li>
     <!-- Delete User Ajax logic -->
