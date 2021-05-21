d8d68bceba76941f5e52449108acb9592b830fe8
ivan santos <isantos.santos255@gmail.com>
2021-05-21 14:39:36 +0200

    test

--- Space_project/resources/views/FrontOffice/booking/booking.blade.php
+++ Space_project/resources/views/FrontOffice/booking/booking.blade.php
@@ -1,18 +1,26 @@
 @extends('templates/template')
 <!-- Style -->
 @section('style')
+<<<<<<< Updated upstream
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 <link href="{{ asset('css/booking.css') }}" rel="stylesheet">
 
+=======
+<link href="{{ asset('css/booking.css') }}" rel="stylesheet">
+>>>>>>> Stashed changes
 @endsection
 @section('title', 'Booking')
 
 
 @section('content')
 <h2>All pakage for the user </h2>
+<div class="bigcard">
 
-@if(count($allFligth) > 0)
+    @if(count($allFligth) > 0)
 
+    @foreach($allFligth as $fligth)
+
+<<<<<<< Updated upstream
 @foreach($allFligth as $fligth)
 <div class="bigcard">
     <div class="card mycard" style="width: 18rem;">
@@ -23,14 +31,25 @@
         <div class="card-body bodyCard">
             <h5 class="card-title">Pakage to {{$fligth->itinerary}}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Fly Ref: {{$fligth->fly_ref}}</h6>
+=======
+    <div class="card mycard" style="width: 18rem;">
+        <div class="imag">
+            <img src="uploads/{{$fligth->file}}" class="card-img-top" alt="...">
+        </div>
+        <div class="card-body bodyCard">
+            <h3 class="card-title">Pakage to {{$fligth->itinerary}}</h3>
+            <h5 class="card-subtitle mb-2 text-muted">Fly Ref: {{$fligth->fly_ref}}</h5>
+>>>>>>> Stashed changes
             <p class="card-text"><span>Depart :</span> {{$fligth->depart_date}} at {{$fligth->depart_time}}</p>
             <a href="{{ url('/bookingDitail', $fligth->id) }}">Details</a>
         </div>
     </div>
 
+
+
+
+    @endforeach
+    @endif
+
 </div>
-
-
-@endforeach
-@endif
 @endsection
--- Space_project/resources/views/about.blade.php
+++ Space_project/resources/views/about.blade.php
@@ -2,23 +2,24 @@
 
 <!-- Style -->
 @section('style')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> -->
 <link href="{{ asset('css/about.css') }}" rel="stylesheet">
 @endsection
 @section('title', 'About us')
 
 </head>
+
 <body>
-@section('content')
+  @section('content')
 
 
 
 
 
-@endsection
+  @endsection
 
-<!-- Main backround with planet -->
-<!-- <div class="container__main">
+  <!-- Main backround with planet -->
+  <!-- <div class="container__main">
 <div class="planet"> 
   <div class="wrap">
      <div class="background"></div>
@@ -37,12 +38,13 @@
 
 
 
-<!-- Title -->
-<!-- <h1 class="home__title">TOGETHER WE OPEN SPACE FOR CHANGE MIND</h1> -->
+  <!-- Title -->
+  <!-- <h1 class="home__title">TOGETHER WE OPEN SPACE FOR CHANGE MIND</h1> -->
 
-<!-- Packages card from Bootstrap -->
-<!-- Earth Rotation https://codepen.io/stephaniewalter/pen/vYGmrLR  -->
+  <!-- Packages card from Bootstrap -->
+  <!-- Earth Rotation https://codepen.io/stephaniewalter/pen/vYGmrLR  -->
 
 
 </body>
+
 </html>
