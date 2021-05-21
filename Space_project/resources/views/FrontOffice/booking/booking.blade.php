6908a32257b20acbfd7a24b60a48fadf3acf8b65
ivan santos <isantos.santos255@gmail.com>
2021-05-21 14:38:12 +0200

    com

--- Space_project/resources/views/FrontOffice/booking/booking.blade.php
+++ Space_project/resources/views/FrontOffice/booking/booking.blade.php
@@ -2,6 +2,7 @@
 <!-- Style -->
 @section('style')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link href="{{ asset('css/booking.css') }}" rel="stylesheet">
 
 @endsection
 @section('title', 'Booking')
@@ -13,10 +14,13 @@
 @if(count($allFligth) > 0)
 
 @foreach($allFligth as $fligth)
-<div>
-    <div class="card" style="width: 18rem;">
-        <img src="uploads/{{$fligth->file}}" class="card-img-top" alt="...">
-        <div class="card-body">
+<div class="bigcard">
+    <div class="card mycard" style="width: 18rem;">
+        <div class="imag">
+            <img src="uploads/{{$fligth->file}}" class="card-img-top" alt="...">
+
+        </div>
+        <div class="card-body bodyCard">
             <h5 class="card-title">Pakage to {{$fligth->itinerary}}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Fly Ref: {{$fligth->fly_ref}}</h6>
             <p class="card-text"><span>Depart :</span> {{$fligth->depart_date}} at {{$fligth->depart_time}}</p>
