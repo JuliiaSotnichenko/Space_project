2a674c792f2f6a3c69892d59efdc6c317e31b247
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2021-05-21 09:47:51 +0200

    update

--- Space_project/resources/views/BackOffice/bop-template.blade.php
+++ Space_project/resources/views/BackOffice/bop-template.blade.php
@@ -5,27 +5,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Add style link -->
-    <!-- No bootstrap anymore <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title>@yield('title')</title>
 </head>
 <body>
 
 
+<<nav class="navbar navbar-expand-lg navbar-light bg-light">
 <nav class="navbar">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">StarFlight</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
     <div class="navbar-container" id="navbarRegister">
       <ul class="navbar-nav">
         <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Users</a>
           <a class="nav-link" href="#">Users</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Bookings</a>
           <a class="nav-link" href="#">Bookings</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Flights</a>
           <a class="nav-link" href="#">Flights</a>
         </li>
         <li class="nav-item">
@@ -33,6 +39,8 @@
         </li>
       </ul>
       <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
         <input class="form-control" type="search" placeholder="Search" aria-label="Search">
         <button class="btn" type="submit">Search</button>
       </form>
@@ -44,7 +52,7 @@
 
 @yield('footer-backend')
 
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script> -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
 
 </body>
