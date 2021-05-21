7050c6d039673c6e4ef485e81d737e9841ada8bd
Steve Weidig <75318450+Bofferbrauer@users.noreply.github.com>
2021-05-21 10:48:51 +0200

    favicon

--- Space_project/resources/views/BackOffice/bop-template.blade.php
+++ Space_project/resources/views/BackOffice/bop-template.blade.php
@@ -51,7 +51,7 @@
 
   @yield('content')
 
-  @yield('footer-backend')
+  @yield('footer')
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
