992727d155a6dbbbeaa698ed393a15214291bba1
ivan santos <isantos.santos255@gmail.com>
2021-05-21 14:48:43 +0200

    test

--- Space_project/resources/views/FrontOffice/booking/booking.blade.php
+++ Space_project/resources/views/FrontOffice/booking/booking.blade.php
@@ -10,12 +10,10 @@
 
 @section('content')
 <h2>All pakage for the user </h2>
-<div class="bigcard">
 
-    @if(count($allFligth) > 0)
+@if(count($allFligth) > 0)
 
-    @foreach($allFligth as $fligth)
-
+<<<<<<< HEAD
     <<<<<<< Updated upstream @foreach($allFligth as $fligth) <div class="bigcard">
         <div class="card mycard" style="width: 18rem;">
             <div class="imag">
@@ -47,3 +45,26 @@
 
             </div>
             @endsection
+=======
+@foreach($allFligth as $fligth)
+<div class="bigcard">
+    <div class="card mycard" style="width: 18rem;">
+        <div class="imag">
+            <img src="uploads/{{$fligth->file}}" class="card-img-top" alt="...">
+
+        </div>
+        <div class="card-body bodyCard">
+            <h5 class="card-title">Pakage to {{$fligth->itinerary}}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Fly Ref: {{$fligth->fly_ref}}</h6>
+            <p class="card-text"><span>Depart :</span> {{$fligth->depart_date}} at {{$fligth->depart_time}}</p>
+            <a href="{{ url('/bookingDitail', $fligth->id) }}">Details</a>
+        </div>
+    </div>
+
+</div>
+
+
+@endforeach
+@endif
+@endsection
+>>>>>>> parent of d8d68bc (test)
