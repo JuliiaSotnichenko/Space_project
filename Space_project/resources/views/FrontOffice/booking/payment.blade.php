803523bed45234d0a5d387ea15e7cdc1211059fe
yuliia_sotnichenko <juliia.sotnichenko@gmail.com>
2021-05-21 15:33:44 +0200

    created payment page and some form

--- Space_project/resources/views/FrontOffice/booking/payment.blade.php
+++ Space_project/resources/views/FrontOffice/booking/payment.blade.php
@@ -1,1 +1,72 @@
-<h2>payment</h2>
+@extends('templates/template')
+
+<!-- Style -->
+@section('style')
+<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> -->
+
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link href="{{ asset('css/payment.css') }}" rel="stylesheet">
+@endsection
+@section('title', 'Payment')
+
+@section('content')
+
+<div class="container">
+  <div id="Checkout" class="inline">
+      <h1>Pay Invoice</h1>
+      <div class="card-row">
+          <span class="visa"></span>
+          <span class="mastercard"></span>
+          <span class="amex"></span>
+          <span class="discover"></span>
+      </div>
+
+
+      <form >
+          <div class="form-group">
+              <h3 class="PaymentAmount">Payment</h3>
+              <!-- <div class="amount-placeholder">
+                  <span>$</span>
+                  <span>500.00</span>
+              </div> -->
+          </div>
+          <div class="form-group">
+              <label or="NameOnCard">Name on card</label>
+              <input id="NameOnCard" class="form-control" type="text" maxlength="255"></input>
+          </div>
+          <div class="form-group">
+              <label for="CreditCardNumber">Card number</label>
+              <input id="CreditCardNumber" class="null card-image form-control" type="text"></input>
+          </div>
+          <div class="expiry-date-group form-group">
+              <label for="ExpiryDate">Expiry date</label>
+              <input id="ExpiryDate" class="form-control" type="text" placeholder="MM / YY" maxlength="7"></input>
+          </div>
+          <div class="security-code-group form-group">
+              <label for="SecurityCode">Security code</label>
+              <div class="input-container" >
+                  <input id="SecurityCode" class="form-control" type="text" ></input>
+                  <i id="cvc" class="fa fa-question-circle"></i>
+              </div>
+              <div class="cvc-preview-container two-card hide">
+                  <div class="amex-cvc-preview"></div>
+                  <div class="visa-mc-dis-cvc-preview"></div>
+              </div>
+          </div>
+          <div class="zip-code-group form-group">  
+          </div>
+          <button id="PayButton" class="btn btn-block btn-success submit-button" type="submit">
+              <span class="submit-button-lock"></span>
+              <span class="align-middle">Pay</span>
+          </button>
+      </form>
+  </div>
+</div>
+
+@endsection
+
+<!-- Script -->
+@section('script')
+<script src="js/payment.js"></script>
+@endsection
