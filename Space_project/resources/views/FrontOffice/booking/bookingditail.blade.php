5b77d8bf88b5499baf53e02efc3771d84a545409
ivan santos <isantos.santos255@gmail.com>
2021-05-22 12:35:22 +0200

    detail pakage

--- Space_project/resources/views/FrontOffice/booking/bookingditail.blade.php
+++ Space_project/resources/views/FrontOffice/booking/bookingditail.blade.php
@@ -4,7 +4,11 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 <link href="{{ asset('css/booking.css') }}" rel="stylesheet">
 @endsection
+@section('title', 'Pakages |Detail')
 @section('content')
+<div class="titleh2">
+    <h1> Pakages Detail</h1>
+</div>
 <div class="detail">
 
     <div class="carddetail">
@@ -14,32 +18,32 @@
         <div class="desc">
 
             <div>
-                <span>Reference :</span> {{$fligth->fly_ref}}
+                <span>Reference </span> {{$fligth->fly_ref}}
             </div>
 
             <div>
-                <span>Itinerary:</span> {{$fligth->itinerary}}
+                <span>Itinerary </span> {{$fligth->itinerary}}
                 <br>
-                <span>launch Location:</span> {{$fligth->location}}
+                <span>launch Location - </span> {{$fligth->location}}
 
             </div>
             <div>
-                <span>Depart date:</span> {{$fligth->depart_date}}
+                <span>Depart date </span> {{$fligth->depart_date}}
                 <br>
-                <span>Arrival date:</span> {{$fligth->arrival_date}}
+                <span>Arrival date </span> {{$fligth->arrival_date}}
                 <br>
 
             </div>
             <div>
-                <span>Departe time:</span> {{$fligth->depart_time}}
+                <span>Departe time </span> {{$fligth->depart_time}}
                 <br>
-                <span>Arrival time:</span> {{$fligth->arrival_time}}
+                <span>Arrival time </span> {{$fligth->arrival_time}}
             </div>
             <div>
-                <span>Price:</span> {{$fligth->price}}
+                <span>Price </span> {{$fligth->price}}€
             </div>
             <div>
-                <span>Discription:</span> {{$fligth->description}}
+                <span>Discription </span> {{$fligth->description}}
             </div>
         </div>
 
