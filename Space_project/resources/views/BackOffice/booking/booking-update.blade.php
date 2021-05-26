b61a6ee4e7e8defec1715cc58ca843d2c96aa56a
Mwachkiff <machiseverino@gmail.com>
2021-05-26 16:21:47 +0200

    Update booking-update.blade.php

--- Space_project/resources/views/BackOffice/booking/booking-update.blade.php
+++ Space_project/resources/views/BackOffice/booking/booking-update.blade.php
@@ -2,9 +2,38 @@
 
 @section('title', 'Update Booking')
 
+@section('style')
+<style>
+    .success {
+        background-color: #fff;
+        color: #243B7B;
+        position: absolute;
+        top: 200px;
+        transform: translateX(-50%);
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 10px 10px 10px 5px black;
+
+    }
+
+    .success a {
+        padding: 5px 20px;
+        color: white;
+        background-color: blue;
+        border-radius: 10px;
+        text-decoration: none;
+    }
+
+    .success a:hover {
+        box-shadow: 5px 5px 5px black;
+    }
+
+    .success img {
+        width: 80px;
+    }
+</style>
+@endsection
 @section('content')
-
-
 <h2>Update Booking</h2>
 <form action="" method="POST">
     @csrf
