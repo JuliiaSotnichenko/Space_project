08f02072b2217443bc3c98217de4e4ef5bf81224
Vichitaev <78409677+Vichitaev@users.noreply.github.com>
2021-05-26 11:25:12 +0200

    sadadadadada

--- Space_project/resources/views/dashboard.blade.php
+++ Space_project/resources/views/dashboard.blade.php
@@ -79,7 +79,7 @@
         <li>Itinerary: {{ $flight->itinerary}}</li>
         <li>Location: {{ $flight->location}}</li>
         <li>Flight ref.: {{ $flight->fly_ref}}</li>
-        
+
         <div>
             @if( $flight->location == 'NASA Kennedy Space Center')
             <!-- Giving the clients the addresses to the Spaceports -->
@@ -88,79 +88,79 @@
                 FL 32953<br>
                 United States</p>
             @elseif( $flight->location == 'Baikonur Cosmodrome')
-            <p >Abay Avenue 5<br>
+            <p>Abay Avenue 5<br>
                 Baikonur<br>
                 Kazakhstan</p>
             @elseif( $flight->location == 'Guiana Space Center Kourou')
-            <p >Route de l'Espace<br>
+            <p>Route de l'Espace<br>
                 Kourou<br>
-                French Guiana</p> 
-    
+                French Guiana</p>
+
             @endif
         </div>
     </ul>
     <div>
-    <ul>
-        <div></div>
-        <div class='container'>
+        <ul>
+            <div></div>
+            <div class='container'>
+                <hr>
+                <div>
+                    @if( $flight->location == 'NASA Kennedy Space Center')
+                    <!-- weather widget start -->
+                    <a target="_blank" href="https://hotelmix.fr/weather/orlando-19887">
+                        <!-- weather widget start -->
+                        <a target="_blank" href="https://www.booked.net/weather/orlando-19887">
+                            <img src="https://w.bookcdn.com/weather/picture/32_19887_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=&anc_id=48006" alt="booked.net" /></a>
+                        <!-- weather widget end -->
+                        @elseif( $flight->location == 'Baikonur Cosmodrome')
+                        <!-- weather widget start -->
+                        <a target="_blank" href="https://www.booked.net/weather/ayteke-bi-w651588">
+                            <img src="https://w.bookcdn.com/weather/picture/32_w651588_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=&anc_id=48006" alt="booked.net" /></a>
+                        <!-- weather widget end -->
+                        @else
+                        <!-- weather widget start --><a target="_blank" href="https://hotelmix.fr/weather/kourou-17000">
+                            <img src="https://w.bookcdn.com/weather/picture/32_17000_1_3_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=581&anc_id=94940" alt="booked.net" /></a>
+                        <!-- weather widget end -->
+                        @endif
+                </div>
+            </div>
+
             <hr>
-            <div>
-                @if( $flight->location == 'NASA Kennedy Space Center')
-                <!-- weather widget start -->
-                <a target="_blank" href="https://hotelmix.fr/weather/orlando-19887">
-                    <!-- weather widget start -->
-                    <a target="_blank" href="https://www.booked.net/weather/orlando-19887">
-                        <img src="https://w.bookcdn.com/weather/picture/32_19887_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=&anc_id=48006" alt="booked.net" /></a>
-                    <!-- weather widget end -->
-                    @elseif( $flight->location == 'Baikonur Cosmodrome')
-                    <!-- weather widget start -->
-                    <a target="_blank" href="https://www.booked.net/weather/ayteke-bi-w651588">
-                        <img src="https://w.bookcdn.com/weather/picture/32_w651588_1_1_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=&anc_id=48006" alt="booked.net" /></a>
-                    <!-- weather widget end -->
-                    @else
-                    <!-- weather widget start --><a target="_blank" href="https://hotelmix.fr/weather/kourou-17000">
-                        <img src="https://w.bookcdn.com/weather/picture/32_17000_1_3_34495e_250_2c3e50_ffffff_ffffff_1_2071c9_ffffff_0_6.png?scode=124&domid=581&anc_id=94940" alt="booked.net" /></a>
-                    <!-- weather widget end -->
-                    @endif
+            <!-- Google maps-->
+            <div></div>
+
+            <div class="mapouter">
+                <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q={{ $flight->location}}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2piratebay.org">pirate bay</a><br>
+                    <style>
+                        .mapouter {
+                            position: relative;
+                            text-align: right;
+                            height: 500px;
+                            width: 600px;
+                        }
+                    </style><a href="https://www.embedgooglemap.net">google map html generator</a>
+                    <style>
+                        .gmap_canvas {
+                            overflow: hidden;
+                            background: none !important;
+                            height: 500px;
+                            width: 600px;
+                        }
+                    </style>
+                </div>
             </div>
-        </div>
-        
-        <hr>
-        <!-- Google maps-->
-        <div></div>
-
-        <div class="mapouter">
-            <div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q={{ $flight->location}}&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2piratebay.org">pirate bay</a><br>
-                <style>
-                    .mapouter {
-                        position: relative;
-                        text-align: right;
-                        height: 500px;
-                        width: 600px;
-                    }
-                </style><a href="https://www.embedgooglemap.net">google map html generator</a>
-                <style>
-                    .gmap_canvas {
-                        overflow: hidden;
-                        background: none !important;
-                        height: 500px;
-                        width: 600px;
-                    }
-                </style>
-            </div>
-        </div>
-        </div>
-        </div>
+    </div>
+</div>
 
 
-        @endif
-        @endif
+@endif
+@endif
 
 
 
-        @endauth
+@endauth
 
-                </ul>
+</ul>
 </div>
 
 
