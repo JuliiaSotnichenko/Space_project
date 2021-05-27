e5ad8591ccfc6975eed4cd6e3423e8c3b8bee862
yuliia_sotnichenko <juliia.sotnichenko@gmail.com>
2021-05-27 13:49:41 +0200

    Search

--- Space_project/resources/views/FrontOffice/booking/booking.blade.php
+++ Space_project/resources/views/FrontOffice/booking/booking.blade.php
@@ -1,4 +1,5 @@
 @extends('templates/template')
+<meta name="_token" content="{{ csrf_token() }}">
 <!-- Style -->
 @section('style')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
@@ -8,6 +9,36 @@
 
 
 @section('content')
+
+
+<div class="container">
+<div class="row">
+<div class="panel panel-default">
+<div class="panel-heading">
+<h3>Packages info </h3>
+</div>
+<div class="panel-body">
+    <div class="form-group">
+        <input type="text" class="form-controller" id="search" name="search"></input>
+    </div>
+<table class="table table-bordered table-hover">
+<thead>
+<tr>
+<th>ID</th>
+<th>Packages Name</th>
+<th>Accomodation</th>
+<th>Price</th>
+</tr>
+</thead>
+<tbody>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</div>
+
+
 
 @if(count($allFligth) > 0)
 <div class="titleh2">
@@ -28,6 +59,28 @@
     </div>
     @endforeach
 </div>
-
 @endif
+@endsection
+
+
+<script type="text/javascript">
+$('#search').on('keyup',function(){
+$value=$(this).val();
+$.ajax({
+type : 'get',
+url : "{{URL('/search')}}",
+data:{'search':$value},
+success:function(data){
+$('tbody').html(data);
+}
+});
+})
+</script>
+<script type="text/javascript">
+$.ajaxSetup({ headers: { 'csrftoken' : '{{ csrf_token() }}' } });
+</script>
+
+
+@section('script')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 @endsection
--- Space_project/resources/views/search.blade.php
+++ Space_project/resources/views/search.blade.php
@@ -49,5 +49,6 @@ $('tbody').html(data);
 <script type="text/javascript">
 $.ajaxSetup({ headers: { 'csrftoken' : '{{ csrf_token() }}' } });
 </script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 </body>
 </html>
